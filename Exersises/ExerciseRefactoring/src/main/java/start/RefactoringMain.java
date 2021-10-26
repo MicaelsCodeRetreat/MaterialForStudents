9b39ba81f6f1906c9af8056f833b8c2b124879fa
Micael Andersson <>
2021-10-26 09:55:23 +0100

    ExerciseRefactoring - renamed dinsdale to donald

--- Exersises/ExerciseRefactoring/src/main/java/start/RefactoringMain.java
+++ Exersises/ExerciseRefactoring/src/main/java/start/RefactoringMain.java
@@ -3,7 +3,7 @@ package start;
 public class RefactoringMain {
 
     public static void main(String[] args) {
-        Customer dinsdale = new Customer("Donald Duck");
+        Customer donald = new Customer("Donald Duck");
         Movie movie1 = new Movie("Monty Python and the Holy Grail", Movie.REGULAR);
         Movie movie2 = new Movie("Star Trek 27                   ", Movie.NEW_RELEASE);
         Movie movie3 = new Movie("Star Wars                      ", Movie.REGULAR);
@@ -12,11 +12,11 @@ public class RefactoringMain {
         Rental rental2 = new Rental(movie2, 3);
         Rental rental3 = new Rental(movie3, 4);
         Rental rental4 = new Rental(movie4, 6);
-        dinsdale.addRental(rental1);
-        dinsdale.addRental(rental2);
-        dinsdale.addRental(rental3);
-        dinsdale.addRental(rental4);
-        String record = dinsdale.statement();
+        donald.addRental(rental1);
+        donald.addRental(rental2);
+        donald.addRental(rental3);
+        donald.addRental(rental4);
+        String record = donald.statement();
         System.out.println(record);
     }
 }
